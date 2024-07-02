4620446eb08384215a43c7564ca63dd35f9a2eef
ClaudiaJulian <claudia_e_julian@yahoo.com.ar>
2024-07-02 11:32:23 -0300

    actualiza funcionalidad desencriptado y funciones genericas

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("Trabajo de encriptado");
 
-/** 
+/** REGLA PARA ENCRIPTAR
 La letra "e" es convertida para "enter"
 La letra "i" es convertida para "imes"
 La letra "a" es convertida para "ai"
@@ -10,27 +10,61 @@ La letra "u" es convertida para "ufat"
 
 
 
-// ENCRIPTAR EL TEXTO INGRESADO
 
-let textoAEncriptar = "hola Andrea";
+let textoATransformar = "";
+let resultadoBusqueda = "";
 let arrayTexto = [];
-let resultadoEncriptado = "";
+let arrayEncriptado = [];
+let reglaParaEncriptar = {
+    "a":"ai",
+    "e":"enter",
+    "i":"imes",
+    "o":"ober",
+    "u":"ufat",
+}
 
-function encriptarTexto(texto){
-    arrayTexto = texto.split('');
-    console.log(arrayTexto);
-    return arrayTexto;
+// GENERICAS
+
+function innerHTML(id,texto){
+    document.getElementById(id).innerHTML= texto;
+};
+
+function valueHTML(id){
+    return value = document.getElementById(id).value;
+}
+
+
+// ENCRIPTA TEXTO
+function encriptarTexto(){
+    textoATransformar = valueHTML("ingreso__textarea");;
+    arrayTexto = textoATransformar.split('');
+
+    for(let i = 0; i < arrayTexto.length; i++) {
+        if (reglaParaEncriptar.hasOwnProperty(arrayTexto[i])){
+            arrayEncriptado.push(reglaParaEncriptar[arrayTexto[i]]);
+        }else{
+            arrayEncriptado.push(arrayTexto[i]);
+        }
+    };    
+    resultadoBusqueda = arrayEncriptado.join('');
+    innerHTML("resultado__encriptado",resultadoBusqueda);
  }
 
-
- // MOSTRARLO EN EL PANEL DE RESULTADO
-
-resultadoEncriptado = document.getElementById("resultado__encriptado");
-resultadoEncriptado.innerHTML= "hola claudia";
-
    
-
 // DESENCRIPTAR EL TEXTO 
 
+function desencriptarTexto(){
+    textoATransformar = valueHTML("ingreso__textarea");
 
+    for (let letra in reglaParaEncriptar) {
+        let clave = reglaParaEncriptar[letra];
+        let regex = new RegExp(clave, "g");       
+        textoATransformar = textoATransformar.replace(regex, letra);
+    }
+     
+    innerHTML("resultado__encriptado", textoATransformar);
+};
+
+
+// API portapapeles
 // VALIDAR QUE NO HAYA MAYUSCULAS NI ACENTOS
