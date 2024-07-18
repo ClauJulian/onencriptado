4b93bb808201084b69c5d05b1e2428b5e05a0566
ClaudiaJulian <claudia_e_julian@yahoo.com.ar>
2024-07-18 17:56:43 -0300

    Incorpora función Copiar y Pegar

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ La letra "u" es convertida para "ufat"
 
 let textoATransformar = "";
 let resultadoBusqueda = "";
+let texto = "";
 let arrayTexto = [];
 let arrayEncriptado = [];
 let reglaParaEncriptar = {
@@ -31,8 +32,20 @@ function innerHTML(id,texto){
 
 function valueHTML(id){
     return value = document.getElementById(id).value;
-}
+};
 
+function displayHTML(id, especificacion){
+    document.getElementById(id).style.display = especificacion;
+};
+
+// MOSTRAR RESULTADO Y CAMBIAR ELEMENTOS
+
+function mostrarResultado(resultado){
+    displayHTML("resultado__imagen",'none');
+    displayHTML("panel__resultado__mensaje",'none');
+    displayHTML("button__copiar", 'inline');
+    innerHTML("resultado__encriptado",resultado);
+}
 
 // ENCRIPTA TEXTO
 function encriptarTexto(){
@@ -47,7 +60,7 @@ function encriptarTexto(){
         }
     };    
     resultadoBusqueda = arrayEncriptado.join('');
-    innerHTML("resultado__encriptado",resultadoBusqueda);
+    mostrarResultado(resultadoBusqueda);
  }
 
    
@@ -55,16 +68,36 @@ function encriptarTexto(){
 
 function desencriptarTexto(){
     textoATransformar = valueHTML("ingreso__textarea");
-
-    for (let letra in reglaParaEncriptar) {
-        let clave = reglaParaEncriptar[letra];
-        let regex = new RegExp(clave, "g");       
-        textoATransformar = textoATransformar.replace(regex, letra);
-    }
-     
-    innerHTML("resultado__encriptado", textoATransformar);
+        for (let letra in reglaParaEncriptar) {
+            let clave = reglaParaEncriptar[letra];
+            let regex = new RegExp(clave, "g");       
+            textoATransformar = textoATransformar.replace(regex, letra);
+        }
+    
+    mostrarResultado(textoATransformar);
 };
 
 
-// API portapapeles
+// API CLIPBOARD
+
+document.getElementById('button__copiar').addEventListener('click', function() {
+    texto = document.getElementById('resultado__encriptado').textContent;
+
+    navigator.clipboard.writeText(texto).then(function() {
+        alert('Texto copiado al portapapeles');
+    }).catch(function(error) {
+        console.error('Error al copiar el texto: ', error);
+    });
+
+    document.getElementById('ingreso__textarea').addEventListener('focus', function() {
+        navigator.clipboard.readText().then(function(texto) {
+            document.getElementById('ingreso__textarea').value = texto;            
+        }).catch(function(error) {
+            console.error('Error al leer el texto del portapapeles: ', error);
+        });
+    });    
+
+});
+
+
 // VALIDAR QUE NO HAYA MAYUSCULAS NI ACENTOS
